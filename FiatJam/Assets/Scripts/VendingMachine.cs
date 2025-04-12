f77726ce4f4f9c92d5b198c52c166a3ebbd02edd
quislingbomb <jasontomjiang@gmail.com>
2025-04-12 03:20:13 -0700

    randomize Carrots

--- FiatJam/Assets/Scripts/VendingMachine.cs
+++ FiatJam/Assets/Scripts/VendingMachine.cs
@@ -7,15 +7,34 @@ public class VendingMachine : Interactable
 
     [SerializeField] private GameObject vendingMachineCanvas;
 
+    public int numCarrots = 0;
+
+    [SerializeField] private List<GameObject> carrotPrefabs = new List<GameObject>();
+
+
+
 
     protected override void Start()
     {
+
         base.Start();
+
+        numCarrots = Random.Range(0, 10);
 
         if (vendingMachineCanvas != null)
         {
             vendingMachineCanvas.SetActive(false);
         }
+
+        for (int i = 0; i < carrotPrefabs.Count; i++)
+        {
+            carrotPrefabs[i].SetActive(false);
+        }
+
+
+
+        DisplayCarrots();
+
     }
 
     public override void Interact()
@@ -58,6 +77,18 @@ public class VendingMachine : Interactable
                     player.SetMovementEnabled(true);
                 }
             }
+        }
+    }
+
+
+    public void DisplayCarrots()
+    {
+        for (int i = 0; i < numCarrots; i++)
+        {
+            carrotPrefabs[i].SetActive(true);
+
+
+
         }
     }
 }
