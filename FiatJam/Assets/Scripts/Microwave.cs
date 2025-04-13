b97d4cd528fc799c1cc72c72a0dc55e21e374e27
quislingbomb <jasontomjiang@gmail.com>
2025-04-12 18:43:18 -0700

    microwave item swapping
    f-ck microwaves

--- FiatJam/Assets/Scripts/Microwave.cs
+++ FiatJam/Assets/Scripts/Microwave.cs
@@ -6,7 +6,7 @@ using UnityEngine.EventSystems;
 
 public class Microwave : Interactable
 {
-    // Start is called before the first frame update
+
 
 
     [SerializeField] private GameObject microwaveCanvas;
@@ -14,7 +14,14 @@ public class Microwave : Interactable
     [SerializeField] private List<GameObject> inventorySlots =  new List<GameObject>();
 
 
-    [SerializeField] public GameObject microwaveItem;
+    [SerializeField] private List<GameObject> inventoryButtons = new List<GameObject>();
+    [SerializeField] public GameObject microwaveItem = null;
+    [SerializeField] private GameObject microwaveButton;
+
+    [SerializeField] private GameObject microwaveItemPrefab;
+
+    private Vector3 originalPrefabPosition;
+
 
 
 
@@ -29,31 +36,166 @@ public class Microwave : Interactable
         }
 
 
+        for (int i = 0; i < inventoryButtons.Count; i++)
+        {
+            int index = i;
+            Button button = inventoryButtons[i].GetComponent<Button>();
+            if (button != null)
+            {
+                Debug.Log($"Adding listener to button {index}");
+                button.onClick.AddListener(() => OnInventoryButtonClicked(index));
+            }
+            else
+            {
+                Debug.LogWarning($"Button component missing on inventoryButtons[{i}]");
+            }
+        }
+        if (microwaveButton != null){
+            microwaveButton.transform.SetAsLastSibling();
+            Button button = microwaveButton.GetComponent<Button>();
+            if (button != null)
+            {
+                button.onClick.AddListener(OnMicrowaveButtonClicked);
+            }
+        }
+
+        if (microwaveItemPrefab != null)
+        {
+            RectTransform prefabRectTransform = microwaveItemPrefab.GetComponent<RectTransform>();
+            if (prefabRectTransform != null)
+            {
+                originalPrefabPosition = prefabRectTransform.localPosition;
+                Debug.Log($"Original prefab position saved: {originalPrefabPosition}");
+            }
+        }
+
 
     }
 
-    public void OnItemClicked()
-    {   
 
-        Debug.Log("Item clicked:");
+    private void UpdateInventoryDisplay(Player player)
+    {
+        if (player != null)
+        {
+            for (int i = 0; i < inventorySlots.Count; i++)
+            {
+                Image slotImage = inventorySlots[i].GetComponent<Image>();
+                if (slotImage != null)
+                {
+                    if (i < player.inventory.Count && player.inventory[i] != null)
+                    {
 
-        
+                        SpriteRenderer itemSpriteRenderer = player.inventory[i].GetComponent<SpriteRenderer>();
+                        if (itemSpriteRenderer != null)
+                        {
+                            slotImage.sprite = itemSpriteRenderer.sprite;
+                            slotImage.color = new Color(1, 1, 1, 1);
+                        }
+                    }
+                    else
+                    {
 
-
+                        slotImage.sprite = null;
+                        slotImage.color = new Color(1, 1, 1, 0);
+                    }
+                }
+            }
+        }
     }
 
+    public void OnInventoryButtonClicked(int index)
+    {
+        Debug.Log($"Inventory button {index} clicked.");
+
+        GameObject playerObject = GameObject.FindGameObjectWithTag("Player");
+        if (playerObject != null)
+        {
+            Player player = playerObject.GetComponent<Player>();
+            if (player != null && index >= 0 && index < player.inventory.Count)
+            {
+                GameObject inventoryItem = player.inventory[index];
+
+                if (inventoryItem == null && microwaveItem == null)
+                {
+
+                    Debug.Log("Both inventory slot and microwave are empty. No action performed.");
+                    return;
+                }
+
+                if (microwaveItem != null)
+                {
+
+                    GameObject temp = microwaveItem;
+                    microwaveItem = inventoryItem;
+                    player.inventory[index] = temp;
+                }
+                else
+                {
+
+                    microwaveItem = inventoryItem;
+                    player.inventory[index] = null; 
+                }
 
 
+                UpdateMicrowaveDisplay();
 
+
+                UpdateInventoryDisplay(player);
+            }
+        }
+    }
+
+    public void OnMicrowaveButtonClicked()
+    {
+        if (microwaveItem != null)
+        {
+            GameObject playerObject = GameObject.FindGameObjectWithTag("Player");
+            if (playerObject != null)
+            {
+                Player player = playerObject.GetComponent<Player>();
+                if (player != null)
+                {
+
+                    bool itemAdded = false;
+                    for (int i = 0; i < player.inventory.Count; i++)
+                    {
+                        if (player.inventory[i] == null) 
+                        {
+                            player.inventory[i] = microwaveItem;
+                            Debug.Log($"Added {microwaveItem.name} to inventory slot {i}.");
+                            itemAdded = true;
+                            break;
+                        }
+                    }
+
+                    if (!itemAdded)
+                    {
+                        Debug.LogWarning("No empty inventory slots available. Item not added.");
+                    }
+
+                    microwaveItem = null;
+
+
+                    UpdateMicrowaveDisplay();
+
+
+                    UpdateInventoryDisplay(player);
+                }
+            }
+        }
+        else
+        {
+            Debug.Log("Microwave is empty, nothing to add to inventory.");
+        }
+    }
 
     public override void Interact()
     {
         Debug.Log("*beep* *beep* *beep*");
 
-        if (microwaveCanvas != null){
+        if (microwaveCanvas != null)
+        {
             microwaveCanvas.SetActive(true);
-
-
 
             GameObject playerObject = GameObject.FindGameObjectWithTag("Player");
 
@@ -63,57 +205,84 @@ public class Microwave : Interactable
                 if (player != null)
                 {
                     player.SetMovementEnabled(false);
+
+
+                    UpdateInventoryDisplay(player);
                 }
-
-                List<GameObject> inventory = player.inventory;
-
-                for (int i = 0; i < 5; i++){
-                    Image slotImage = inventorySlots[i].GetComponent<Image>();
-                    if (slotImage != null)
-                    {
-                        slotImage.sprite = null;
-                        slotImage.color = new Color(1, 1, 1, 0);
-
-                    }
-                }
-
-                for (int i = 0; i < inventory.Count; i++)
-                {
-                    if (i < 5){
-
-                        Image slotImage = inventorySlots[i].GetComponent<Image>();
-                        if (slotImage != null)
-                        {
-                            SpriteRenderer itemSprite = inventory[i].GetComponent<SpriteRenderer>();
-
-                            if (itemSprite != null)
-                            {
-                                slotImage.sprite = itemSprite.sprite;
-                                slotImage.color = new Color(1, 1, 1, 1);
-                            }
-                        }
-                        else
-                        {
-                            Debug.LogWarning("Item does not have a Sprite Renderer");
-                        }
-                    }
-                }
-
-
-
-
-
-
-
             }
+
+ 
+            UpdateMicrowaveDisplay();
         }
     }
 
-    
+    private void UpdateMicrowaveDisplay()
+    {
+        if (microwaveItem != null)
+        {
+            Debug.Log("Microwave item is not null. Attempting to update display.");
 
 
+            microwaveItemPrefab.SetActive(true);
 
 
+            SpriteRenderer microwaveSpriteRenderer = microwaveItem.GetComponent<SpriteRenderer>();
+
+            Image prefabImage = microwaveItemPrefab.GetComponent<Image>();
+
+            if (microwaveSpriteRenderer != null)
+            {
+                Debug.Log($"Microwave item sprite found: {microwaveSpriteRenderer.sprite?.name ?? "No sprite assigned"}");
+            }
+            else
+            {
+                Debug.LogWarning("Microwave item does not have a SpriteRenderer component.");
+            }
+
+            if (prefabImage != null)
+            {
+                Debug.Log("Microwave prefab Image component found.");
+            }
+            else
+            {
+                Debug.LogWarning("Microwave prefab does not have an Image component.");
+            }
+
+
+            if (microwaveSpriteRenderer != null && prefabImage != null)
+            {
+                prefabImage.sprite = microwaveSpriteRenderer.sprite;
+                prefabImage.color = new Color(1, 1, 1, 1); 
+
+                if (prefabImage.sprite != null)
+                {
+                    Debug.Log($"Microwave prefab image successfully updated to: {prefabImage.sprite.name}");
+                }
+                else
+                {
+                    Debug.LogWarning("Microwave prefab image is null after update.");
+                }
+            }
+
+
+            RectTransform prefabRectTransform = microwaveItemPrefab.GetComponent<RectTransform>();
+            if (prefabRectTransform != null)
+            {
+                prefabRectTransform.localPosition = originalPrefabPosition;
+                Debug.Log($"Microwave prefab local position reset to original: {prefabRectTransform.localPosition}");
+            }
+            else
+            {
+                Debug.LogWarning("Microwave prefab does not have a RectTransform component.");
+            }
+        }
+        else
+        {
+            Debug.Log("Microwave item is null. Hiding the microwaveItemPrefab.");
+
+            microwaveItemPrefab.SetActive(false);
+        }
+    }
     public void CloseCanvas()
     {
         if (microwaveCanvas != null){
